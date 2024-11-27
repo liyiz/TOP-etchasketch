b0a156d6a6078d6a6e02f49595df9eb2bd409832
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-26 19:40:37 -0500

    Append one div

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+document.addEventListener('DOMContentLoaded',() => {
+    // handle DOMContentLoaded event
+    createGrid();
+});
+
+function createGrid() {
+    // create rows
+    // create columns
+    const container = document.querySelector('#container');
+
+    
+    const gridCell = document.createElement('div');
+
+    container.appendChild(gridCell);
+
+}
