3a0ebb94bda886d12e6890914d1e39f4db1bf130
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-26 20:09:11 -0500

    Initial cell generation and functionality

--- script.js
+++ script.js
@@ -11,10 +11,19 @@ function createGrid() {
     // create columns
     const container = document.querySelector('#container');
 
+    // set grid size
+    // set cell size
+
+    // generate cells
+
     for (let i = 0; i < gridX; i++) {
         for (let j = 0; j < gridY; j++) {
             const gridCell = document.createElement('div');
-            gridCell.classList.add('cell');
+            gridCell.classList.add('cell', 'fill');
+            gridCell.classList.toggle('fill'); // initially flip off the fill
+            gridCell.addEventListener('mouseenter', () => { 
+                gridCell.classList.toggle('fill'); 
+            });
             container.appendChild(gridCell);
         }
     }
