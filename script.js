9c09a1d836bd9577bf70bd866078c36db31a7ab3
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-27 09:46:06 -0500

    Add reset button

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-const gridX = 16;
-const gridY = 16;
+const defaultSize = 16;
 
 document.addEventListener('DOMContentLoaded',() => {
     // handle DOMContentLoaded event
-    createGrid();
+    createGrid(defaultSize, defaultSize);
+
+    const resetBtn = document.querySelector('#reset');
+    resetBtn.addEventListener('click', () => {
+        let gridX = prompt('Enter size of x axis', defaultSize);
+        while (isNaN(gridX) || gridX > 512) {
+            alert('You did not enter a number or you tried a number larger than 512, try again');
+            gridX = prompt('Enter size of x axis', defaultSize);
+        }
+        let gridY = prompt('Enter size of y axis ', defaultSize);
+        while (isNaN(gridY) || gridY > 512) {
+            alert('You did not enter a number or you tried a number larger than 512, try again');
+            gridY = prompt('Enter size of y axis', defaultSize);
+        }
+        
+        createGrid(gridX, gridY);
+    })
 });
 
-function createGrid() {
+function createGrid(gridX, gridY) {
     // create rows
     // create columns
     const container = document.querySelector('#container');
-
+    
+    // reset previous grid
+    container.innerHTML = '';
     // set grid size
     // set cell size
 
@@ -19,14 +36,20 @@ function createGrid() {
     for (let i = 0; i < gridX; i++) {
         for (let j = 0; j < gridY; j++) {
             const gridCell = document.createElement('div');
+            css( gridCell, { width: `calc(100%/${gridX})`, height: `calc(100%/${gridY})` });
             gridCell.classList.add('cell', 'fill');
             gridCell.classList.toggle('fill'); // initially flip off the fill
             gridCell.addEventListener('mouseenter', () => { 
-                gridCell.classList.toggle('fill'); 
+                gridCell.classList.add('fill'); 
             });
             container.appendChild(gridCell);
         }
     }
 
+}
 
+
+function css(e, styles) {
+    for (const property in styles)
+        e.style[property] = styles[property];
 }
