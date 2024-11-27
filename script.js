eb35033d9a637c037b99d85aad1e9531e7c100a3
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-26 19:46:56 -0500

    Add cell and container styles

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const gridX = 16;
+const gridY = 16;
+
 document.addEventListener('DOMContentLoaded',() => {
     // handle DOMContentLoaded event
     createGrid();
@@ -8,9 +11,13 @@ function createGrid() {
     // create columns
     const container = document.querySelector('#container');
 
+    for (let i = 0; i < gridX; i++) {
+        for (let j = 0; j < gridY; j++) {
+            const gridCell = document.createElement('div');
+            gridCell.classList.add('cell');
+            container.appendChild(gridCell);
+        }
+    }
 
-    const gridCell = document.createElement('div');
-    gridCell.classList.add('cell');
-    container.appendChild(gridCell);
 
 }
